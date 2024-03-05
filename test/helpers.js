f5d2271382f8b93981b2f703787aba64485e3c90
Eric <ericgorza@gmail.com>
2024-03-05 14:15:49 -0800

    first test ready

--- test/helpers.js
+++ test/helpers.js
@@ -1,8 +1,17 @@
 const supertest = require('supertest');
-const chai = require('chai');
+// const chai = require('chai');
 const app = require('../index.js');
 const config = require('../config.js');
 global.app = app;
 global.request = supertest(app);
-global.expect = chai.expect;
-global.config = config;
+// global.expect = chai.expect;
+// global.config = config;
+
+import('chai').then((chai) => {
+    global.expect = chai.expect;
+    global.request = supertest(app);
+
+    // Restante do seu código aqui
+  }).catch((err) => {
+    console.error('Erro ao importar chai:', err);
+  });
