2e424fe51a5af9283f7e00bb7b3025550dccef88
Eric <ericgorza@gmail.com>
2024-03-05 16:01:10 -0800

    tasks test tem que debugar

---
+++ jwt.js
@@ -0,0 +1,6 @@
+module.exports = {
+    jwt: {
+        secret: 'Nta$K-AP1',
+        options: { session: false }
+    }
+};
--- routes/tasks.js
+++ routes/tasks.js
@@ -1,21 +1,3 @@
-// module.exports = app => {
-//     app.get('/tasks', (req, res) => {
-//       res.json({
-//         tasks: [
-//           { title: 'Fazer compras' },
-//           { title: 'Consertar o pc' }
-//         ]
-//   }); });
-//   };
-
-// module.exports = app => {
-//   const Tasks = app.models.tasks;
-//   app.get('/tasks', (req, res) => {
-//     Tasks.findAll({}, (tasks) => {
-//       res.json({ tasks });
-//     });
-// }); };
-
 module.exports = app => {
   const Tasks = app.models.tasks;
 
