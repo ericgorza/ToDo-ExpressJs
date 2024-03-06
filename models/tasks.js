491f86280aeff316a1384111920fb2ca9fe2af0a
Eric <ericgorza@gmail.com>
2024-03-05 16:28:06 -0800

    testes prontos

--- models/tasks.js
+++ models/tasks.js
@@ -1,29 +1,3 @@
-// const { DataTypes } = require('sequelize');
-
-// module.exports = (app) => {
-//   const Tasks = app.db.define('Tasks', {
-//     id: {
-//       type: DataTypes.INTEGER,
-//       primaryKey: true,
-//       autoIncrement: true
-//     },
-//     title: {
-//       type: DataTypes.STRING,
-//       allowNull: false,
-//       validate: {
-//         notEmpty: true
-//       }
-//     },
-//     done: {
-//       type: DataTypes.BOOLEAN,
-//       allowNull: false,
-//       defaultValue: false
-//     }
-//   });
-
-//   return Tasks;
-// };
-
 const { DataTypes } = require('sequelize');
 
 module.exports = (app) => {
@@ -47,10 +21,9 @@ module.exports = (app) => {
     }
   });
 
-  // Defina as associações dentro da função associate
+
   Tasks.associate = (models) => {
     Tasks.belongsTo(models.Users, { foreignKey: 'userId' });
-    // Outras associações conforme necessário
   };
 
   return Tasks;
