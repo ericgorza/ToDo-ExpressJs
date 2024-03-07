94f036dd139927c5d83b54e7ae575d5c215ab2e7
Eric <ericgorza@gmail.com>
2024-03-07 12:15:52 -0800

    logs adicionados

--- middlewares.js
+++ middlewares.js
@@ -1,9 +1,21 @@
 const bodyParser = require('body-parser');
 const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const logger = require('./logger');
 
 module.exports = app => {
   app.set('port', 3000);
   app.set('json spaces', 4);
+  app.use(morgan('common', {
+    stream: {
+      write: (log) => logger.info(log)
+  } }));
+  app.use(cors({
+    origin: ['http://localhost:3001'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+  }));
   app.use(bodyParser.json());
   app.use(app.auth.initialize());
   app.use((req, res, next) => {
